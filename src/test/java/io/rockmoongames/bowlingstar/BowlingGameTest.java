6bee650c32227d535df4c6017b16f9eb6a3ccb4a
Pawel Kozubala <p.kozubala@gmail.com>
2021-10-25 06:01:58 +0200

    first commit

---
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -0,0 +1,21 @@
+package io.rockmoongames.bowlingstar;
+
+import org.junit.jupiter.api.Test;
+
+public class BowlingGameTest {
+
+    @Test
+    public void jessicaTestOne() {
+
+    }
+
+    @Test
+    public void jessicaTestTwo() {
+
+    }
+
+    @Test
+    public void jessicaTestThree() {
+
+    }
+}
